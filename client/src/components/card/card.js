01c444f0211a9ebc157017cfe43be51089192868
Rochita <69753947+rochita-sharma@users.noreply.github.com>
2024-12-17 00:30:38 +0530

    added introduction text with card hover animation

--- client/src/components/card/card.js
+++ client/src/components/card/card.js
@@ -4,10 +4,10 @@ import './card.css';
 function Card() {
     return (
         <div className='introCard'>
-            {/* <div className='image'></div> */}
-            <div className='introduction'>
-              
-            </div>
+              <div id='greetings'>Hello! <span id='wavingHello'>👋</span></div>
+              <div id='Iam'>I'm</div>
+              <div id='introName'>Rochita</div>
+              <div id='introLine'>A Software Developer and a keen passionate learner who loves to implement the real world ideas.</div>
         </div>
     );
 }
