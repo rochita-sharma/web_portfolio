8c70beb2d0c4c8bf9727863e941e05619c9b1534
Rochita <69753947+rochita-sharma@users.noreply.github.com>
2024-12-21 22:46:47 +0530

    card: intro and css animations applied

---
+++ client/src/components/Projects/projects.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import './projects.css';
+
+function Projects() {
+
+}
+
+export default Projects;
--- client/src/components/card/card.js
+++ client/src/components/card/card.js
@@ -4,9 +4,8 @@ import './card.css';
 function Card() {
     return (
         <div className='introCard'>
-              <div className='greetingHello' id='greetings'>Hello! <span class="wave">👋</span></div>
-              <div id='Iam'>I'm</div>
-              <div id='introName'>Rochita</div>
+              <div id='greetings'>Hello!<span id="wave">👋</span>, I am</div>
+              <div id='introName' className='anim-typewriter'>Rochita</div>
               <div id='introLine'>A Software Developer and a keen passionate learner who loves to implement the real world ideas.</div>
         </div>
     );
